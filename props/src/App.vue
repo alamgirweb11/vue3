ec730fa05e55270c86ff1e695079c840f28f8677
alamgirweb11 <alamgirweb11@gmail.com>
2022-02-25 12:10:54 +0600

    basic uses of props

---
+++ props/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+   <h3 class="base-title">Props</h3>
+  <user :name="name" :age="age"></user>
+ </div>
+</template>
+
+<script>
+import User from './components/User.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+              name: "Demo",
+              age: 26
+    }
+  },
+  components: {
+    User
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  $base-width: 80%;
+  $heading-color: #000;
+  $color: #1b1a1a;
+  $heading-font-size: 18px;
+  @mixin heading-style(){
+           color: $heading-color;
+           font-size: $heading-font-size;
+           text-align: left;
+  }
+   .container{
+        width: $base-width;
+        margin: 0 auto;
+   }
+   .base-title{
+       @include heading-style();
+   }
+</style>
---
+++ props/src/components/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="user-wrapper">
+     <h4>Your name is  {{  name }} and your are {{ age }} years old.</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserComponent',
+  props: ["name", "age"]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
