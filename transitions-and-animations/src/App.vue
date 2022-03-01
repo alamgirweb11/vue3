0c4a13ddac6b8641430389cc9f1a371e6161452a
alamgirweb11 <alamgirweb11@gmail.com>
2022-03-01 23:48:04 +0600

    css animations added

--- transitions-and-animations/src/App.vue
+++ transitions-and-animations/src/App.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="container">
         <button @click="flag = !flag" class="toggle-btn">Toggle</button>
-         <transition name="fade" mode="out-in">
+         <!-- <transition name="fade" mode="out-in">
             <h2 v-if="flag" class="heading-title" key="base">Hello World!</h2>
             <h2 v-else class="heading-title" key="sub">Hello Visitors!</h2>
+         </transition> -->
+         <!-- css animations -->
+         <transition name="zoom" type="animation" appear>
+            <h2 v-if="flag" class="css-animation-block">Hello World!</h2>
          </transition>
     </div>
 </template>
@@ -15,7 +19,7 @@ export default {
   name: 'App',
   data() {
     return {
-          flag: false,
+          flag: true,
     }
   },
   components: {
@@ -48,4 +52,43 @@ export default {
      transition: all 0.5s linear;
      opacity: 0;
   }
+
+/* css animations style */
+.zoom-enter-active{
+     animation: zoom-in 1s linear forwards;
+     transition: all 2s linear;
+}
+.zoom-leave-active{
+ animation: zoom-out 1s linear forwards;
+  transition: all 2s linear;
+}
+
+.zoom-enter-from{
+     opacity: 0;
+}
+.zoom-leave-to{
+     opacity: 0;
+}
+
+.css-animation-block{
+     width: 400px;
+     padding: 20px;
+     margin: 20px;
+}
+@keyframes zoom-in{
+     from{
+        transform: scale(0, 0);
+     }
+     to{
+         transform: scale(1, 1);
+     }
+}
+@keyframes zoom-out{
+     from{
+        transform: scale(1, 1);
+     }
+     to{
+         transform: scale(0, 0);
+     }
+}
 </style>
