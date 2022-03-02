0157e0705bedf9c50be29f0be05d3daa8e0ca6c4
alamgirweb11 <alamgirweb11@gmail.com>
2022-03-02 23:55:32 +0600

    js animation added

--- transitions-and-animations/src/App.vue
+++ transitions-and-animations/src/App.vue
@@ -6,8 +6,20 @@
             <h2 v-else class="heading-title" key="sub">Hello Visitors!</h2>
          </transition> -->
          <!-- css animations -->
-         <transition name="zoom" type="animation" appear>
+         <!-- <transition name="zoom" type="animation" appear>
             <h2 v-if="flag" class="css-animation-block">Hello World!</h2>
+         </transition> -->
+           <!-- javascript animations -->
+         <transition 
+         @before-enter = "beforeEnter" 
+         @enter = "enter"
+         @after-enter = "afterEnter"
+         @before-leave = "beforeLeave"
+         @leave = "leave"
+         @after-leave = "afterLeave"
+         :css="false"
+        >
+            <h2 v-if="flag" class="js-animation-block">Hello World!</h2>
          </transition>
     </div>
 </template>
@@ -24,7 +36,42 @@ export default {
   },
   components: {
 
-  }
+  },
+  methods: {
+        beforeEnter(el){
+            console.log('before-enter event fired',  el)
+        },
+        enter(el, done){
+           console.log('enter event fired',  el)
+           const animation = el.animate([{ transform: "scale3d(0,0,0)" }, {}], {
+              duration: 1000,
+           })
+
+           animation.onfinish = () => {
+               done()
+           }
+        },
+        afterEnter(el){
+            console.log('after-enter event fired',  el)
+        },
+        beforeLeave(el){
+             console.log('before-leave event fired',  el)
+        },
+        leave(el, done){
+            console.log('leave event fired',  el)
+
+              const animation = el.animate([{}, {transform: "scale3d(0,0,0)" }], {
+              duration: 1000,
+           })
+
+           animation.onfinish = () => {
+               done()
+           }
+        },
+        afterLeave(el){
+          console.log('after-leave event fired',  el)
+        },
+  },
 }
 </script>
 
