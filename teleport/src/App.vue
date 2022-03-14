2e4ff71f47922ba7c56740733c812a1b340ee54a
alamgirweb11 <alamgirweb11@gmail.com>
2022-03-14 23:58:45 +0600

    teleport components

---
+++ teleport/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+       <CommentForm/>
+    
+    </div>
+</template>
+
+<script>
+import CommentForm from '/CommentForm.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+        
+    }
+  },
+  components: {
+CommentForm
+  },
+  methods: {
+  
+  },
+}
+</script>
+
+<style>
+  .container{
+     width: 80%;
+     margin: 0 auto;
+  }
+  
+</style>
---
+++ teleport/src/components/CommentForm.vue
@@ -0,0 +1,14 @@
+<template lang="">
+   <div class="comments-form">
+      <textarea v-model="comments" placeholder="Write your comments"></textarea>
+      <button>Submit</button>
+   </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
